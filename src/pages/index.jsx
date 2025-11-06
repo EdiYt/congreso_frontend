739d5ccd6bddbcc1417111d8543212d48d9b9d36
Erick <erickvillaseor@yahoo.com.mx>
2025-11-05 18:28:41 -0600

    Ya estan todas las rutas pero no se si bien

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./Home.jsx";
+import Home from "./Home";
+import ParticipantesList from "./ParticipantesList";
+import RegistroParticipante from "./RegistroParticipante";
+import Gafete from "./Gafete";
+import NotFound from "./NotFound";
+import LayoutPublic from "../layout/LayoutPublic";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <LayoutPublic />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "participantes",
+        element: <ParticipantesList />,
+      },
+      {
+        path: "registro",
+        element: <RegistroParticipante />,
+      },
+      {
+        path: "gafete/:id",
+        element: <Gafete />,
+      },
+    ],
   },
-
 ]);
