c9260e41177e78e389245e668310b0312adc2b1f
Erick <erickvillaseor@yahoo.com.mx>
2025-11-06 16:33:37 -0600

    Al recargar ya sale el anuncio

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './pages/index.jsx'
+import ReloadPrompt from './ReloadPrompt'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ReloadPrompt />
     <RouterProvider router={router} />
   </StrictMode>
 )
