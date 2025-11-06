e19245873d9adcc568481780344a3f35b7766f12
Erick <erickvillaseor@yahoo.com.mx>
2025-11-05 22:55:32 -0600

    Se agrego el home junto con su propio css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import universidadImg from "../assets/universidad.png";
+import ticImg from "../assets/tic.png";
+import "../styles/home.css";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  const handleEntrar = () => {
+    navigate("/participantes");
+  };
+
+  return (
+    <div className="home-bg">
+      <div className="home-logos-row">
+        <img src={universidadImg} alt="Logo Universidad" className="home-logo-img" />
+        <img src={ticImg} alt="Logo Congreso TICs" className="home-logo-img" />
+      </div>
+      <h1 className="home-title">Congreso TICs Full Stack</h1>
+      <button className="btn btn-primary btn-lg mt-3 shadow" onClick={handleEntrar}>
+        Entrar
+      </button>
+    </div>
+  );
+};
+
+export default Home;
